7524205af2eeca175d83709309b611418dd91f11
rootx <learnwithrootx@gmail.com>
2026-05-23 02:50:19 +0300

    chore: update package author metadata to Rootx

--- ppm/__init__.py
+++ ppm/__init__.py
@@ -1,5 +1,5 @@
 """PPM - Python Package Manager"""
 
 __version__ = "1.0.2"
-__author__ = "PPM Authors"
+__author__ = "Rootx"
 __description__ = "Smart Python environment and package management CLI"
